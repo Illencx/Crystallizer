f95f69aff10c2b6a265211033b72bed29a8de66b
Nero <nerocheng@lucasgc.com>
2024-02-27 18:41:14 +0800

    add lombok & constant

--- src/main/java/dev/illencx/crystallizer/Crystallizer.java
+++ src/main/java/dev/illencx/crystallizer/Crystallizer.java
@@ -1,6 +1,7 @@
 package dev.illencx.crystallizer;
 
 import dev.illencx.crystallizer.item.TestItem;
+import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
@@ -10,25 +11,30 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import static dev.illencx.crystallizer.constant.CrystallizerConstant.MOD_ID;
+import static dev.illencx.crystallizer.constant.CrystallizerConstant.NAME_SPACE;
 
+/**
+ * @author Illencx
+ * @Date 2024/2/27
+ * @version 1.0.0
+ */
+
+@Slf4j
 public class Crystallizer implements ModInitializer {
-    public static final String MOD_ID ="crystallizer";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final Item NEWITEM =new TestItem(new Item.Settings().maxCount(64));
+    public static final Item NEWITEM = new TestItem(new Item.Settings().maxCount(64));
 
     private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(NEWITEM))
-            .displayName(Text.of("crystallizer"))
+            .displayName(Text.of(NAME_SPACE))
             .entries((context, entries) -> entries.add(NEWITEM))
             .build();
+
     @Override
     public void onInitialize() {
-        LOGGER.info("onInitialize");
-        LOGGER.info(MOD_ID);
-        Registry.register(Registries.ITEM, new Identifier("crystallizer", "test_item"), NEWITEM);
-        Registry.register(Registries.ITEM_GROUP, new Identifier("crystallizer", "test_group"), ITEM_GROUP);
+        log.info("onInitialize, mod id : {}", MOD_ID);
+        Registry.register(Registries.ITEM, new Identifier(NAME_SPACE, "test_item"), NEWITEM);
+        Registry.register(Registries.ITEM_GROUP, new Identifier(NAME_SPACE, "test_group"), ITEM_GROUP);
     }
 }
---
+++ src/main/java/dev/illencx/crystallizer/constant/CrystallizerConstant.java
@@ -0,0 +1,14 @@
+package dev.illencx.crystallizer.constant;
+
+/**
+ * @author Nero Claudius
+ * @version 1.0.0
+ * @since 2024/2/27
+ */
+public interface CrystallizerConstant {
+
+    String MOD_ID = "crystallizer";
+
+    String NAME_SPACE = "crystallizer";
+
+}
